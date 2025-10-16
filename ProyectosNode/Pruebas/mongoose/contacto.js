eb119a775172718016721c61a44a3e601909fcdc
= <=>
2025-10-16 12:33:33 +0200

    llibresMongoose ejercicios 1 y 2

---
+++ ProyectosNode/Pruebas/mongoose/contacto.js
@@ -0,0 +1,25 @@
+import { mongoose } from "mongoose";
+
+const contactoSchema = new mongoose.Schema({
+  nombre: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true,
+  },
+  telefono: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 1,
+    trim: true,
+    match: /^\d{9}$/,
+  },
+  edad: {
+    type: Number,
+    min: 18,
+    max: 120,
+  },
+});
+
+export const contacto = mongoose.model("contactos", contactoSchema);
---
+++ ProyectosNode/Pruebas/mongoose/index.js
@@ -0,0 +1,6 @@
+import { mongoose } from "mongoose";
+import { contacto } from "./contacto.js";
+
+mongoose.connect("mongodb://localhost:27017/contactos");
+
+
